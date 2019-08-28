dbcaae61581d37f965302413f390786257343d09
chhaya909 <54603991+chhaya909@users.noreply.github.com>
2019-08-27 20:27:41 -0400

    first commit#1

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client(endpointConfigurationName: "BasicHttpBinding_IService1");
+            label2.Text = client.HelloService(textBox1.Text);
+        }
+    }
+}
